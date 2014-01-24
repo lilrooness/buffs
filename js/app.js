ad02c8442de61ef8654af34615b1072f959b2956
lilrooness <jfrangoudes@gmail.com>
2014-01-24 15:55:35 +0000

    added room for buff alerts

--- js/app.js
+++ js/app.js
@@ -13,5 +13,13 @@ setInterval(function(){
 	
 	$('#blue p').text(moment(moment().diff(blueBuff)).format('mm:ss'));
 	$('#red p').text(moment(moment().diff(redBuff)).format('mm:ss'));
+
+	if(moment().diff(blueBuff, 'minutes') >= 5) {
+		//TODO Make alert
+	}
+
+	if(moment().diff(redBuff, 'minutes') >= 5) {
+		//TODO Make alert
+	}
 	
 }, 1000);
